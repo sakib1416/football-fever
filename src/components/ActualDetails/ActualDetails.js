0cfd76bc2c40a7445776b7ec2a4820cd4f97d377
sakib1416 <sakib1416@gmail.com>
2021-03-11 12:08:19 +0100

    redirect

--- src/components/ActualDetails/ActualDetails.js
+++ src/components/ActualDetails/ActualDetails.js
@@ -3,14 +3,16 @@ import React from 'react';
 
 
 const ActualDetails = (props) => {
-    let {strLeague, strDescriptionEN, strGender, intFormedYear, strSport, strTwitter, strFacebook, strYoutube, strCountry} = props.league;
+    let {strLeague, strBanner, strLogo, strDescriptionEN, strGender, intFormedYear, strSport, strTwitter, strFacebook, strYoutube, strCountry} = props.league;
     console.log(strTwitter, strFacebook, strYoutube);
     // strGender = 'Female'
     const male = 'https://raw.githubusercontent.com/ProgrammingHero1/sports-mania/main/Photo/male.png';
     const female = 'https://raw.githubusercontent.com/ProgrammingHero1/sports-mania/main/Photo/female.png';
+    
     return (
         <div>
             <h1>League: {strLeague}</h1>
+            <img src={strBanner} alt=""/>
             <p>Founded: {intFormedYear}</p>
             <p>Country: {strCountry}</p>
             <p>Sport type: {strSport}</p>
